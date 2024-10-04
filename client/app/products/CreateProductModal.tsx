b2e4ebf7e882ceca58f11cfa7169c75c16cb61a3
wongmb <michaelwong3090@gmail.com>
2024-10-04 18:54:26 -0400

    Initial Commit

---
+++ client/app/products/CreateProductModal.tsx
@@ -0,0 +1,135 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { v4 } from "uuid";
+import Header from "@/app/(components)/Header";
+
+type ProductFormData = {
+  name: string;
+  price: number;
+  stockQuantity: number;
+  rating: number;
+};
+
+type CreateProductModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onCreate: (formData: ProductFormData) => void;
+};
+
+const CreateProductModal = ({
+  isOpen,
+  onClose,
+  onCreate,
+}: CreateProductModalProps) => {
+  const [formData, setFormData] = useState({
+    productId: v4(),
+    name: "",
+    price: 0,
+    stockQuantity: 0,
+    rating: 0,
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onCreate(formData);
+    onClose();
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]:
+        name === "price" || name === "stockQuantity" || name === "rating"
+          ? parseFloat(value)
+          : value,
+    });
+  };
+
+  if (!isOpen) return null;
+
+  const labelCssStyles = "block text-sm font-medium text-gray-700";
+  const inputCssStyles =
+    "block w-full mb-2 p-2 border-gray-500 border-2 rounded-md";
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
+      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+        <Header name="Create New Product" />
+        <form onSubmit={handleSubmit} className="mt-5">
+          {/*Product Name*/}
+          <label htmlFor="productName" className={labelCssStyles}>
+            Product Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+            value={formData.name}
+            className={inputCssStyles}
+            required
+          />
+
+          {/*Price*/}
+          <label htmlFor="productPrice" className={labelCssStyles}>
+            Product Name
+          </label>
+          <input
+            type="number"
+            name="price"
+            placeholder="Price"
+            onChange={handleChange}
+            value={formData.price}
+            className={inputCssStyles}
+            required
+          />
+
+          {/*Stock Quantity*/}
+          <label htmlFor="stockQuantity" className={labelCssStyles}>
+            Stock Quantity
+          </label>
+          <input
+            type="number"
+            name="stockQuantity"
+            placeholder="Stock Quantity"
+            onChange={handleChange}
+            value={formData.stockQuantity}
+            className={inputCssStyles}
+            required
+          />
+
+          {/*Rating*/}
+          <label htmlFor="rating" className={labelCssStyles}>
+            Rating
+          </label>
+          <input
+            type="number"
+            name="rating"
+            placeholder="Rating"
+            onChange={handleChange}
+            value={formData.rating}
+            className={inputCssStyles}
+            required
+          />
+
+          {/* Create Actions */}
+          <button
+            type="submit"
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          >
+            Create
+          </button>
+          <button
+            onClick={onClose}
+            type="button"
+            className="ml-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
+          >
+            Cancel
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateProductModal;
---
+++ server/src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/* ROUTE IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
+import expenseRoutes from "./routes/expenseRoutes";
+
+
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes);
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes);
+
+
+
+/* SERVER */
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
+});
+
